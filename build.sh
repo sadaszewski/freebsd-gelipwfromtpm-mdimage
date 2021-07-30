ef62bc965de83ca71e2b459431c4656cb6040417
sadaszewski <s.adaszewski@gmail.com>
2021-07-30 15:31:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,94 +1,55 @@
-.
-./.snap
-./bin
-./bin/sh
-./bin/ls
-./bin/kenv
-./bin/cat
-./boot
-./dev
-./etc
-./etc/rc
-./lib
-./lib/libedit.so.8
-./lib/libc.so.7
-./lib/libncursesw.so.9
-./lib/libgeom.so.5
-./lib/libutil.so.9
-./lib/libbsdxml.so.4
-./lib/libsbuf.so.6
-./lib/libthr.so.3
-./lib/libcrypto.so.111
-./lib/libkiconv.so.4
-./lib/geom
-./lib/geom/geom_eli.so
-./lib/libmd.so.6
-./libexec
-./libexec/ld-elf.so.1
-./sbin
-./sbin/init
-./sbin/geli
-./sbin/mount
-./sbin/mount_msdosfs
-./sbin/reboot
-./tmp
-./usr
-./usr/local
-./usr/local/bin
-./usr/local/bin/tpm2_nvread
-./usr/local/bin/tpm2_dictionarylockout
-./usr/local/lib
-./usr/local/lib/libtss2-esys.a
-./usr/local/lib/libtss2-esys.so
-./usr/local/lib/libtss2-esys.so.0
-./usr/local/lib/libtss2-esys.so.0.0.0
-./usr/local/lib/libtss2-fapi.a
-./usr/local/lib/libtss2-fapi.so
-./usr/local/lib/libtss2-fapi.so.1
-./usr/local/lib/libtss2-fapi.so.1.0.0
-./usr/local/lib/libtss2-mu.a
-./usr/local/lib/libtss2-mu.so
-./usr/local/lib/libtss2-mu.so.0
-./usr/local/lib/libtss2-mu.so.0.0.0
-./usr/local/lib/libtss2-rc.a
-./usr/local/lib/libtss2-rc.so
-./usr/local/lib/libtss2-rc.so.0
-./usr/local/lib/libtss2-rc.so.0.0.0
-./usr/local/lib/libtss2-sys.a
-./usr/local/lib/libtss2-sys.so
-./usr/local/lib/libuuid.a
-./usr/local/lib/libtss2-sys.so.1
-./usr/local/lib/libtss2-sys.so.1.0.0
-./usr/local/lib/libtss2-tcti-cmd.a
-./usr/local/lib/libtss2-tcti-cmd.so
-./usr/local/lib/libtss2-tcti-cmd.so.0
-./usr/local/lib/libtss2-tcti-cmd.so.0.0.0
-./usr/local/lib/libtss2-tcti-device.a
-./usr/local/lib/libtss2-tcti-device.so
-./usr/local/lib/libtss2-tcti-device.so.0
-./usr/local/lib/libtss2-tcti-device.so.0.0.0
-./usr/local/lib/libtss2-tcti-mssim.a
-./usr/local/lib/libtss2-tcti-mssim.so
-./usr/local/lib/libtss2-tcti-mssim.so.0
-./usr/local/lib/libtss2-tcti-mssim.so.0.0.0
-./usr/local/lib/libtss2-tcti-pcap.a
-./usr/local/lib/libtss2-tctildr.a
-./usr/local/lib/libtss2-tcti-pcap.so
-./usr/local/lib/libtss2-tcti-pcap.so.0
-./usr/local/lib/libtss2-tcti-pcap.so.0.0.0
-./usr/local/lib/libtss2-tcti-swtpm.a
-./usr/local/lib/libtss2-tcti-swtpm.so
-./usr/local/lib/libtss2-tcti-swtpm.so.0
-./usr/local/lib/libtss2-tcti-swtpm.so.0.0.0
-./usr/local/lib/libtss2-tctildr.so
-./usr/local/lib/libtss2-tctildr.so.0
-./usr/local/lib/libuuid.so
-./usr/local/lib/libuuid.so.1
-./usr/local/lib/libuuid.so.1.2
-./usr/local/lib/libtss2-tctildr.so.0.0.0
-./usr/bin
-./usr/bin/truss
-./usr/lib
-./usr/lib/libsysdecode.a
-./usr/lib/libsysdecode.so
-./usr/lib/libsysdecode.so.5
+#!/bin/sh
+
+WORKDIR="$1"
+IMGFNAM="$2"
+
+mkdir -p ${WORKDIR}/bin
+mkdir -p ${WORKDIR}/boot
+mkdir -p ${WORKDIR}/dev
+mkdir -p ${WORKDIR}/etc
+mkdir -p ${WORKDIR}/lib/geom
+mkdir -p ${WORKDIR}/libexec
+mkdir -p ${WORKDIR}/sbin
+mkdir -p ${WORKDIR}/tmp
+mkdir -p ${WORKDIR}/usr/local/bin
+mkdir -p ${WORKDIR}/usr/local/lib
+mkdir -p ${WORKDIR}/usr/bin
+mkdir -p ${WORKDIR}/usr/lib
+
+for FNAM in sh ls kenv cat; do
+  cp -v /bin/${FNAM} ${WORKDIR}/bin/
+done
+
+cp -v ./rc ${WORKDIR}/etc/
+
+for FNAM in libedit libc libncursesw libgeom libutil libbsdxml libsbuf libthr libcrypto libkiconv libmd; do
+  cp -v /lib/${FNAM}.so* ${WORKDIR}/lib/
+done
+
+cp -v /lib/geom/geom_eli.so ${WORKDIR}/lib/geom/
+
+cp -v /libexec/ld-elf.so* ${WORKDIR}/libexec/
+
+for FNAM in init geli mount mount_msdosfs reboot; do
+  cp -v /sbin/${FNAM} ${WORKDIR}/sbin/
+done
+
+for FNAM in tpm2_nvread tpm2_dictionarylockout; do
+  cp -v /usr/local/bin/${FNAM} ${WORKDIR}/usr/local/bin/
+done
+
+cp -a /usr/local/lib/libtss2-* ${WORKDIR}/usr/local/lib/
+
+for FNAM in libuuid; do
+  cp -v /usr/local/lib/${FNAM}.so* ${WORKDIR}/usr/local/lib/
+done
+
+for FNAM in truss; do
+  cp -v /usr/bin/${FNAM} ${WORKDIR}/usr/bin/
+done
+
+for FNAM in libsysdecode; do
+  cp -v /usr/lib/${FNAM}.so* ${WORKDIR}/usr/lib/
+done
+
+makefs ${WORKDIR} ${IMGFNAM}
